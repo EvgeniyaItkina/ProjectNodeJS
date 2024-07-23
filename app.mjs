6a29d443cb6db7dcdd8d712f08fe496abe3cf919
Evgeniya Itkina <eva.last.ita@gmail.com>
2024-07-23 10:50:55 +0300

    add custom format for morgan add colors chalk

--- app.mjs
+++ app.mjs
@@ -1,7 +1,15 @@
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import moment from 'moment';
 import morgan from 'morgan';
+import fs from 'fs';
+import path from 'path';
+import { createStream } from 'rotating-file-stream';
+import { fileURLToPath } from 'url';
+import chalk from 'chalk';
+
+export const app = express();
 
 async function main() {
   await mongoose.connect('mongodb://localhost:27017/MyProject');
@@ -9,11 +17,49 @@ async function main() {
 }
 main().catch(err => console.log(err));
 
-export const app = express();
 
 app.use(express.json());
 
-app.use(morgan('combined'))
+// get current url of this file
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+// check if log directory exists
+const logDirectory = path.join(__dirname, 'helpers/logs');
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory, { recursive: true });
+
+// Create log directory if it doesn't exist
+const accessLogStream = createStream((time, index) => {
+  if (!time) return 'access.log';
+  return `${moment(time).format('YYYY_MM_DD')}-access.log`;
+}, {
+  interval: '1d', // check log files every dayeck log files every day
+  path: logDirectory
+});
+
+// custom format `morgan`
+morgan.format('custom', (tokens, req, res) => {
+  return [
+    `Entry Time: ${moment().format('DD/MM/YYYY HH:mm:ss')}`,
+    `Method: ${tokens.method(req, res)}`,
+    `URL: ${tokens.url(req, res)}`,
+    `Response Time: ${tokens['response-time'](req, res)} ms`,
+    `Status: ${tokens.status(req, res)}`
+  ].join('\n');
+});
+
+// Use `morgan` to console with colors
+const consoleFormat = (tokens, req, res) => [
+  chalk.white(`Entry Time: ${moment().format('DD/MM/YYYY HH:mm:ss')}`),
+  chalk.cyan(`Method: ${tokens.method(req, res)}`),
+  chalk.magenta(`URL: ${tokens.url(req, res)}`),
+  chalk.green(`Status: ${tokens.status(req, res)}`),
+  chalk.yellow(`Response Time: ${tokens['response-time'](req, res)} ms`)
+].join('\n');
+app.use(morgan(consoleFormat));
+
+// Use `morgan` to log file
+app.use(morgan('custom', { stream: accessLogStream }));
+
 
 app.use(cors({
   origin: true,
--- helper/logger.mjs
+++
@@ -1,7 +0,0 @@
-import morgan from 'morgan';
-
-export const logger = {
-  log(message) {
-    console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] ${message}`);
-  }
-}
